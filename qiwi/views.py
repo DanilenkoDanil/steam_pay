c3efc1f46bef883b076a9e51aca8728e6e15f28b
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-08-07 13:57:47 +0300

    Add files via upload

--- qiwi/views.py
+++ qiwi/views.py
@@ -16,7 +16,7 @@ def get_key(key: str):
 
 
 def get_setting():
-    return Code.objects.filter(id=1)
+    return Setting.objects.filter(id=1)[0]
 
 
 class GetCodeAPIView(generics.RetrieveAPIView):
@@ -28,13 +28,20 @@ class GetCodeAPIView(generics.RetrieveAPIView):
         key = get_key(code)
         if key is False:
             try:
+                print(code)
+                print(setting.digi_code)
+                print(setting.seller_id)
                 info = check_code(code=code, guid=setting.digi_code, seller_id=setting.seller_id)
-            except:
+                print(info)
+            except Exception as e:
+                print(e)
+                print('no info')
                 return Response(f"Ваш код не действителен", status=status.HTTP_201_CREATED)
             if info['retval'] == 0:
-                value = setting.course * float(info['value'])
+                value = float(setting.course) * float(info['value'])
                 try:
-                    send_steam(info['username'], value, setting.qiwi_code)
+                    print(info['username'], value, setting.qiwi_code)
+                    # send_steam(info['username'], value, setting.qiwi_code)
                     Code.objects.create(code=code, status=True, amount=value, username=info['username'], error='')
                     return Response(f"Ваш код принят", status=status.HTTP_201_CREATED)
                 except Exception as e:
