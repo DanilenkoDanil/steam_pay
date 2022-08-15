aeedb5ac99c9540432f5288f3fc7830ce962a4f4
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-08-15 18:46:04 +0300

    Update settings.py

--- steam_up/settings.py
+++ steam_up/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 SECRET_KEY = key
 
 ALLOWED_HOSTS = ['127.0.0.1', '176.57.214.85']
