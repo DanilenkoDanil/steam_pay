5eb674e3a6b7c3b467880e8d271978a9456188e8
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-08-15 18:19:29 +0300

    Add files via upload

--- steam_up/settings.py
+++ steam_up/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ux+6n1r4fec(7cq7yy=venl0^k0d#a_)ra1iszyopz3o(go5s'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
