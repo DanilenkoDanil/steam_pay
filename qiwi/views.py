275e946f269e2c0aa6a92dedaab91be11c2e14e2
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-11-25 16:04:16 +0200

    Add files via upload

--- qiwi/views.py
+++ qiwi/views.py
@@ -6,6 +6,7 @@ from .send import send_steam
 from .models import Code, Setting, Payment
 from .api import check_code
 import requests
+from django.http import JsonResponse
 
 
 def get_key(key: str):
@@ -73,12 +74,12 @@ class JustPayAPIView(generics.RetrieveAPIView):
         amount = request.query_params.get('amount')
         payment_obj = Payment.objects.create(status=False, amount=amount, username=login, error='')
         try:
-            # send_steam(login, amount, setting.qiwi_code)
+            send_steam(login, amount, setting.qiwi_code)
             print('sssend')
             payment_obj.status = True
             payment_obj.save()
-            return Response(f"Запрос принят", status=status.HTTP_201_CREATED)
+            return JsonResponse(f"Success", status=status.HTTP_201_CREATED)
         except Exception as e:
             payment_obj.error = str(e)
             payment_obj.save()
-            return Response(f"Произошла ошибка - обратитесь к продавцу", status=status.HTTP_201_CREATED)
+            return JsonResponse(f"Failure", status=status.HTTP_400_BAD_REQUEST)
