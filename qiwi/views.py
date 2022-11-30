574b677939229a723dc173530e1895a0528bea16
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-11-30 21:51:10 +0200

    Add files via upload

--- qiwi/views.py
+++ qiwi/views.py
@@ -25,45 +25,6 @@ def get_currency():
     return float(response.json()['RUB_KZT'])
 
 
-class GetCodeAPIView(generics.RetrieveAPIView):
-    queryset = Code.objects.all()
-
-    def retrieve(self, request, *args, **kwargs):
-        setting = get_setting()
-        code = request.query_params.get('uniquecode')
-        key = get_key(code)
-        if key is False:
-            try:
-                info = check_code(code=code, guid=setting.digi_code, seller_id=setting.seller_id)
-            except Exception as e:
-                print(e)
-                print('no info')
-                return Response(f"Ваш код не действителен", status=status.HTTP_201_CREATED)
-            if info['retval'] == 0:
-                if setting.auto_course:
-                    currency = get_currency()
-                else:
-                    currency = float(setting.course)
-                value = currency * float(info['value'])
-                code_obj = Code.objects.create(code=code, status=False, amount=value, username=info['username'],
-                                               error='')
-                try:
-                    send_steam(info['username'], value, setting.qiwi_code)
-                    print('sssend')
-                    code_obj.status = True
-                    code_obj.save()
-                    return Response(f"Ваш код принят", status=status.HTTP_201_CREATED)
-                except Exception as e:
-                    code_obj.error = str(e)
-                    code_obj.save()
-                    return Response(f"Произошла ошибка - обратитесь к продавцу", status=status.HTTP_201_CREATED)
-            else:
-                return Response(f"Ваш код не действителен", status=status.HTTP_201_CREATED)
-
-        else:
-            return Response(f"Ваш код уже обработан", status=status.HTTP_201_CREATED)
-
-
 class JustPayAPIView(generics.RetrieveAPIView):
     queryset = Code.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
