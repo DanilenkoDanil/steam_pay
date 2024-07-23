d249807a79d1d3cd139451d70e2ca5f72246cd60
Danil <danilenkodanil74@gmail.com>
2024-07-23 17:41:51 +0300

    update

--- qiwi/views.py
+++ qiwi/views.py
@@ -63,10 +63,7 @@ class GetCodeAPIView(generics.RetrieveAPIView):
                 print('no info')
                 return Response(f"Ваш код не действителен", status=status.HTTP_201_CREATED)
             if info['retval'] == 0:
-                if setting.auto_course:
-                    currency = get_currency()
-                else:
-                    currency = float(setting.course)
+                currency = float(setting.course)
                 value = currency * float(info['value'])
                 code_obj = Code.objects.create(code=code, status=False, amount=value, username=info['username'],
                                                error='')
